e5a9047c1ec09ce40acfce4f9b6059d498431b28
Karolyi <matej.karolyi@gmail.com>
2016-02-25 09:04:59 +0100

    cviko1

---
+++ cviko1.R
@@ -0,0 +1,11 @@
+#zjisteni cesty pracovniho adresare
+getwd();
+#setwd() i pomoci tlacitka more v menu
+install.packages (c("prob"));
+library ("prob");
+
+#kombinacni cislo
+choose(14, 5);
+
+#dokumentace
+??prob
